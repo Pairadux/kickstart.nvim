e18397e774bec6278354d68858a290f590a84ef3
Pairadux <austincgause@gmail.com>
2024-12-14 23:58:50 -0500

    replace ibl config

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,14 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        opts = {
+            indent = { char = '│' },
+            scope = { char = '│' },
+        },
+        config = function(_, opts)
+            local hooks = require 'ibl.hooks'
+            hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+            require('ibl').setup(opts)
+        end,
+    },
 }
