dcd18f15808afe3dcaf5bdfeb47e3fcf88c5b171
Pairadux <austincgause@gmail.com>
2025-03-26 16:47:17 -0400

    Configure bufferline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -84,7 +84,18 @@ return {
             'nvim-tree/nvim-web-devicons',
         },
         config = function()
-            require('bufferline').setup {}
+            require('bufferline').setup {
+                options = {
+                    buffer_close_icon = '',
+                    close_icon = '',
+                    offsets = {
+                        filetype = 'neo-tree',
+                        text = 'File Explorer',
+                        text_align = 'center',
+                        seperator = true,
+                    },
+                },
+            }
         end,
     },
 
