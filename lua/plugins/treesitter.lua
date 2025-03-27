c0bb9ad8e4100f57773bbc20b6b06b44b1344301
Pairadux <austincgause@gmail.com>
2025-03-26 23:37:55 -0400

    Remove useless comments

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@
 ]]
 
 return {
-	{ -- Highlight, edit, and navigate code
+	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		main = "nvim-treesitter.configs", -- Sets main module to use for opts
