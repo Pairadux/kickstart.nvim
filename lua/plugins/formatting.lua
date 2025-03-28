aa4db682f80dcf31c5b6bacb28fb04070ea82a96
Pairadux <austincgause@gmail.com>
2025-03-28 13:30:45 -0400

    Add more formatters

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -11,25 +11,31 @@ return {
         event = { 'BufWritePre' },
         cmd = { 'ConformInfo' },
         keys = {
-            {
-                'gw',
-                function()
-                    require('conform').format { async = true, lsp_format = 'fallback' }
-                end,
-                mode = '',
-                desc = 'Format buffer',
-            },
+            -- stylua: ignore
+            { 'gw', function() require('conform').format { async = true, lsp_format = 'fallback' } end, mode = '', desc = 'Format buffer', },
         },
         opts = {
-            notify_on_error = false,
+            notify_on_error = true,
             format_on_save = false,
             formatters_by_ft = {
                 lua = { 'stylua' },
-                -- Conform can also run multiple formatters sequentially
-                -- python = { "isort", "black" },
-                --
-                -- You can use 'stop_after_first' to run the first available formatter from the list
-                -- javascript = { "prettierd", "prettier", stop_after_first = true },
+                go = { 'gofumpt' },
+
+                javascript = { 'prettier' },
+                css = { 'prettier' },
+                html = { 'prettier' },
+                svelte = { 'prettier' },
+
+                python = { 'ruff' },
+
+                c = { 'clang-format' },
+                cpp = { 'clang-format' },
+
+                markdown = { 'prettier' },
+
+                sh = { 'shfmt' },
+
+                yaml = { 'prettier' },
             },
         },
     },
