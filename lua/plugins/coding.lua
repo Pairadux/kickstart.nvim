6681e67470de355ccbcf18db11bd3b268e903dfa
Pairadux <austincgause@gmail.com>
2025-03-28 13:29:38 -0400

    Setup blink-cmp

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -17,7 +17,6 @@ return {
         opts = {
             library = {
                 -- Load luvit types when the `vim.uv` word is found
-                { path = 'luvit-meta/library', words = { 'vim%.uv' } },
                 { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
                 { path = 'lazy.nvim', words = { 'LazyVim' } },
                 'nvim-dap-ui',
@@ -33,29 +32,23 @@ return {
             'neovim/nvim-lspconfig',
             'nvim-treesitter/nvim-treesitter',
         },
-        config = function()
-            require('go').setup()
-        end,
+        opts = {},
         event = { 'CmdlineEnter' },
         ft = { 'go', 'gomod' },
         build = ':lua require("go.install").update_all_sync()',
     }, -- }}}
 
-    -- Blink{{{
+    -- Blink {{{
     {
         'saghen/blink.cmp',
-        -- optional: provides snippets for the snippet source
-        dependencies = { 'rafamadriz/friendly-snippets' },
-
-        -- use a release tag to download pre-built binaries
+        enabled = function()
+            return not vim.tbl_contains({ 'markdown', 'txt' }, vim.bo.filetype)
+        end,
         version = '1.*',
-        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-        -- build = 'cargo build --release',
-        -- If you use nix, you can build from source using latest nightly rust with:
-        -- build = 'nix run .#build-plugin',
-
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
+        dependencies = {
+            { 'rafamadriz/friendly-snippets' },
+            { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+        },
         opts = {
             -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
             -- 'super-tab' for mappings similar to vscode (tab to accept)
@@ -69,34 +62,48 @@ return {
             -- C-k: Toggle signature help (if signature.enabled = true)
             --
             -- See :h blink-cmp-config-keymap for defining your own keymap
-            keymap = { preset = 'default' },
-
+            keymap = {
+                preset = 'enter',
+                ['<C-d>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            },
             appearance = {
                 -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
                 -- Adjusts spacing to ensure icons are aligned
-                nerd_font_variant = 'mono',
+                nerd_font_variant = 'normal',
             },
-
-            -- (Default) Only show the documentation popup when manually triggered
-            completion = { documentation = { auto_show = false } },
-
-            -- Default list of enabled providers defined so that you can extend it
-            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            snippets = { preset = 'luasnip' },
+            completion = {
+                menu = { border = 'single' },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                    window = { border = 'single' },
+                },
+                -- ghost_text = { enabled = true },
+            },
+            signature = {
+                enabled = true,
+                window = { border = 'single' },
+            },
             sources = {
                 default = { 'lsp', 'path', 'snippets', 'buffer' },
+                per_filetype = {
+                    lua = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+                },
+                providers = {
+                    lazydev = {
+                        name = 'LazyDev',
+                        module = 'lazydev.integrations.blink',
+                        score_offset = 100,
+                    },
+                },
             },
-
-            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-            --
-            -- See the fuzzy documentation for more information
             fuzzy = { implementation = 'prefer_rust_with_warning' },
         },
         opts_extend = { 'sources.default' },
     }, --}}}
 
-    -- nvim-cmd {{{
+    --[[ nvim-cmp {{{
     { -- Autocompletion
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
@@ -211,5 +218,5 @@ return {
                 },
             }
         end,
-    }, -- }}}
+    }, --}}}]]
 }
